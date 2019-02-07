73a9f81f28a4ad155fb42e780f2da6b71c61f092
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-02-07 01:49:34 -0200

    Tratando o formulário de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,12 +4,35 @@ import './Login.css';
 import tweeterLogo from '../twitter.svg';
 
 export default class Login extends Component {
+
+  state = {
+    username: ''
+  };
+
+  handleInputChange = event => {
+    this.setState({
+      username: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { username } = this.state;
+    if (!username.length) return;
+    localStorage.setItem('@GoTwitter:username', username);
+    this.props.history.push('/timeline');
+  }
+
   render() {
     return (
       <div className='login-wrapper'>
         <img src={tweeterLogo} alt="Logotipo do Tweeter" />
-        <form>
-          <input placeholder="Nome de usuário" />
+        <form onSubmit={this.handleSubmit}>
+          <input
+            value={this.state.username}
+            onChange={this.handleInputChange}
+            placeholder="Nome de usuário"
+          />
           <button type="submit">Entrar</button>
         </form>
       </div>
