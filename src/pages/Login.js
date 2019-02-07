ac7de67a176afbb5c6e01195cd7fe4e794ab81ed
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-02-07 01:26:58 -0200

    Exibindo a tela de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 
+import './Login.css';
+import tweeterLogo from '../twitter.svg';
+
 export default class Login extends Component {
   render() {
-    return <h1>Login</h1>;
+    return (
+      <div className='login-wrapper'>
+        <img src={tweeterLogo} alt="Logotipo do Tweeter" />
+        <form>
+          <input placeholder="Nome de usuário" />
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
